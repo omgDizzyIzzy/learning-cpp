b43d45c65ec4cbf533e03c1f0825b9587df51cb7
Izzy <37433998+omgDizzyIzzy@users.noreply.github.com>
2019-12-31 12:25:24 -0500

    Testing classes
    Testing classes among other things

--- main.cpp
+++ main.cpp
@@ -1,19 +1,18 @@
 /* Constantly updating to make slightly better and look cleaner, this is for learning purposes only for myself to track progression
 It's also my way of "taking notes" as I'm a hands on learning
 
-This is essentially my "Hello World"
-
-*/
+This is essentially my "Hello World" */
 
 #include <iostream>
 #include <fstream>
 #include "functions.hh"
-
+#include "Classes/Numbers.h"
 using namespace std;
 
 int main()
 {
-	years();
+	MyClass obj;
+	obj.years();
 	goodbye();
 	return 0;
 }
