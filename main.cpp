720e8a5c07f3e40639db856a224f80a55d5545af
Izzy <37433998+omgDizzyIzzy@users.noreply.github.com>
2019-12-31 10:13:13 -0500

    Removed system("pause")
    Removed system("pause")

--- main.cpp
+++ main.cpp
@@ -1,22 +1,19 @@
-/* Started on 12/28/2019 at 5:46am - 10am
-
-Constantly updating to make slightly better and look cleaner, this is for learning purposes only for myself to track progression
+/* Constantly updating to make slightly better and look cleaner, this is for learning purposes only for myself to track progression
 It's also my way of "taking notes" as I'm a hands on learning
 
 This is essentially my "Hello World"
 
 */
 
-/* PST TRAVIS! Ignore the lovie shit, not meant for you! xD.... unless *eyes* lol
- Yes I know this is VERY messy */
-
 #include <iostream>
+#include <fstream>
 #include "functions.hh"
+
+using namespace std;
 
 int main()
 {
 	years();
 	goodbye();
-	system("PAUSE");
 	return 0;
 }
