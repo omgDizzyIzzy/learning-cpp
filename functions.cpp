ebc401e4bb9eb44d665ed0b17ff88fae1d6d2b39
Izzy <37433998+omgDizzyIzzy@users.noreply.github.com>
2019-12-31 09:36:22 -0500

    Testing "void" functions
    Testing "void" functions and cleaned up a tiny bit

--- functions.cpp
+++ functions.cpp
@@ -2,14 +2,16 @@
 
 using namespace std;
 
-void years() {
 
+int years() {
 	int year = 2020;
 	int fyear;
+
 	cout << "Izzy M. made this from memory!\n" << "The year will be: " << year << " - TIME FLIES SO FAST!" << endl << flush;
 	jump_question:
 	cout << "What is your favorite year?\n" << endl;
 	cin >> fyear;
+	return 0; // Putting this here because if I don't there seems to be an issue where typing a letter would cause a constant loop
 
 
 	if (fyear >= 2020) {
@@ -28,9 +30,21 @@ void years() {
 		cout << "Don't be silly! - Try another!\n" << flush;
 		goto jump_question;
 	}
-	else {
-		cout << "That's a pretty good year: " << fyear << " - Try another!\n" << endl << flush;
+	else if (fyear <= 2020 && 1993 || fyear <= 1992 && 1989 || fyear <= 1988 && 3) {
+		cout << "That's a pretty good year: " << fyear << "\n" << endl << flush;
 		goto jump_question;
+	}
+}
+
+void goodbye()
+{
+
+	if (1 > 3) {
+		cout << "Hello!\n";
+	}
+
+	else {
+		cout << "\n\n" "This is a seperate function that will write out if breaking from the previous, goodbye! :D\n\n";
 	}
 
 }
