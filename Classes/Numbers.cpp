e23adfd60ce8460804477e63eaadfb116ab1fcf3
Izzy <37433998+omgDizzyIzzy@users.noreply.github.com>
2019-12-31 13:23:09 -0500

    Will only accept integers now
    There was an issue when typing letters would break things, it *should* be gone now

--- Classes/Numbers.cpp
+++ Classes/Numbers.cpp
@@ -1,3 +1,4 @@
+#include <limits>
 #include <iostream>
 #include "Numbers.h"
 using namespace std;
@@ -6,32 +7,34 @@ int Numbers::years()
 {
 	int year = 2020;
 	int fyear;
-
-	cout << "Izzy M. - I'm testing classes in separate files\n" << "The year will be: " << year << " - TIME FLIES SO FAST!" << endl << flush;
+	cout << "The year will be: " << year << " - TIME FLIES SO FAST!" << endl << flush;
 jump_question:
 	cout << "What is your favorite year?\n" << endl;
-	cin >> fyear;
-	return 0; // Putting this here because if I don't there seems to be an issue where typing a letter would cause a constant loop
-
-
+	while (!(cin >> fyear)) {
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "\nPlease type a number, try again!\n";
+			goto jump_question;
+	}
 	if (fyear >= 2020) {
-		cout << fyear << " isn't a year yet silly! - Try another!\n" << flush;
-		goto jump_question;
+		cout << fyear << " isn't a year yet silly! - Try another!\n\n" << flush;
+			goto jump_question;
 	}
 	else if (fyear == 1989) {
-		cout << "That's your birth-year my love! " << fyear << " <3 - Try another!\n" << flush;  // Refering to my love!
-		goto jump_question;
+		cout << "That's your birth-year my love! " << fyear << " <3 - Try another!\n\n" << flush;  // Refering to my love!
+			goto jump_question;
 	}
 	else if (fyear == 1993) {
-		cout << "WOW! How did you know my birth-year was " << fyear << "? I love you!! - Try another!\n" << flush;
-		goto jump_question;
+		cout << "WOW! How did you know my birth-year was " << fyear << "? I love you!! - Try another!\n\n" << flush;
+			goto jump_question;
 	}
 	else if (fyear == 1 || fyear == 2) {
-		cout << "Don't be silly! - Try another!\n" << flush;
-		goto jump_question;
+		cout << "Don't be silly! - Try another!\n\n" << flush;
+			goto jump_question;
 	}
 	else if (fyear <= 2020 && 1993 || fyear <= 1992 && 1989 || fyear <= 1988 && 3) {
-		cout << "That's a pretty good year: " << fyear << "\n" << endl << flush;
-		goto jump_question;
+		cout << "That's a pretty good year: " << fyear << "\n\n" << endl << flush;
+			goto jump_question;
 	}
+	return 0;
 }
